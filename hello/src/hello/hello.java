7a0d311b1d6e6154759e50565bf17f55b2899d5b
hwang <hwang@218.38.137.28>
2021-07-08 00:02:38 +0900

    hello test

---
+++ hello/src/hello/hello.java
@@ -0,0 +1,12 @@
+package hello;
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello");
+		System.out.println("hello world");
+		System.out.print("fuck world");
+		System.out.print("how do you do,I'm okay");
+	}
+}
