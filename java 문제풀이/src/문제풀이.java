7fc52d149bf124333d0757290537e2f129cf73c1
hjs8023 <85880264+hjs8023@users.noreply.github.com>
2021-07-12 22:37:25 +0900

    자바(문제풀이)

---
+++ java 문제풀이/src/문제풀이.java
@@ -0,0 +1,11 @@
+
+public class 문제풀이 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("2 + 5 = " + 2 + 5);
+		System.out.println("2 + 5 =" + (2 + 5));
+		
+	}
+
+}
