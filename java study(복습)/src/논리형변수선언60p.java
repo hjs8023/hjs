2f91cca19009ffe4b2c5d14562900080d30d8023
hjs8023 <85880264+hjs8023@users.noreply.github.com>
2021-07-12 22:36:51 +0900

    JAVA STUDY (복습)

---
+++ java study(복습)/src/논리형변수선언60p.java
@@ -0,0 +1,18 @@
+
+public class 논리형변수선언60p {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean b1 = true;
+		boolean b2 = false;
+		System.out.println(b1); // b1 이 지닌 값 출
+		System.out.println(b2);
+		
+		int num1 = 10;
+		int num2 = 20;
+		System.out.println(num1 < num2); // 20은 10보다 크므로 true
+		System.out.println(num1 > num2); // 10은 20보다 크지않으므로 false
+		
+	}
+
+}
---
+++ java study(복습)/src/블록단위주석.java
@@ -0,0 +1,23 @@
+/*
+ * 파일이름 : BlockComment.java
+ * 작성자 : 홍길동
+ * 작성일 : 2019년 7월 25일
+ * 목적 : System.out.println 메소드의 기능 테스
+ */
+public class 블록단위주석 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	/* 다음은 단순한 정수의 출력 */
+	System.out.println(7);
+	System.out.println(3.15);
+	System.out.println("3 + 5 = " + 8);
+	System.out.println(3.15 + "는 실수입니다");
+	System.out.println("3 + 5" + "의 연산 결과는 8입니다.");
+	
+	/* 다음은 덧셈 결과의 출력 */
+	System.out.println(3 + 5);
+	
+	}
+
+}
