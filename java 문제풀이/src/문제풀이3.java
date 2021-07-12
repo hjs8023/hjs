ccf66e7cc194ae1f12a0dd43a76f9b06264abb95
hjs8023 <85880264+hjs8023@users.noreply.github.com>
2021-07-12 22:37:25 +0900

    자바(문제풀이)

---
+++ java 문제풀이/src/문제풀이3.java
@@ -0,0 +1,15 @@
+
+public class 문제풀이3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(12);
+		System.out.println("1" + 2);
+		System.out.println("1" + "2");
+		System.out.println("12");
+		System.out.println(1 + "2"); // TODO 큰따옴표 사용방법
+		
+		
+	}
+
+}
