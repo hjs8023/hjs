a0e938f9a507a48f3023f515c85ae40616f2853f
hjs8023 <85880264+hjs8023@users.noreply.github.com>
2021-07-13 22:53:44 +0900

    싱수64p(3단원)

---
+++ java study(복습)/src/상수64p.java
@@ -0,0 +1,16 @@
+
+public class 상수64p {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final int MAX_SIZE = 100;
+		final char CONST_CHAR = '상';
+		final int CONST_ASSIGNED;
+			CONST_ASSIGNED = 12; // 할당하지 않았던 상수의 값 할당
+			
+				System.out.println("상수1 :" + MAX_SIZE);
+				System.out.println("상수2 :" + CONST_CHAR);
+				System.out.println("상수3 :" + CONST_ASSIGNED);
+	}
+
+}
