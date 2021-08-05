2e41e132515d4dc230261f9b2e82da21263b4c2c
hjs8023 <rhfemqjrm33@gmail.com>
2021-08-05 22:19:54 +0900

    2021년 8월 5일 혼자공부하는자바.

---
+++ 혼공자/src/sce01/exam03/VariableExchangeExample.java
@@ -0,0 +1,19 @@
+package sce01.exam03;
+
+public class VariableExchangeExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 3;
+		int  y = 5;
+		
+		System.out.println("x:" + x + "y:" + y);
+		
+		int temp = x;
+		x=y;
+		y= temp;
+		
+		System.out.println("x:" + x + "y:" + y);
+	}
+
+}
