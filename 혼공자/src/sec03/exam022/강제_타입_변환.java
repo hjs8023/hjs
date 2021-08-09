e5f1657da73732c3e09b6b5bccc260194af37b5e
hjs8023 <rhfemqjrm33@gmail.com>
2021-08-10 00:22:18 +0900

    2021년 8월9일 혼공자

---
+++ 혼공자/src/sec03/exam022/강제_타입_변환.java
@@ -0,0 +1,22 @@
+package sec03.exam022;
+
+public class 강제_타입_변환 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int intValue = 44032;
+		char charValue = (char) intValue;
+		System.out.println(charValue);
+		
+		
+		long longValue = 500;
+		int value = (int) longValue;
+		System.out.println(intValue);
+		
+		double doubleValue = 3.14;
+		intValue = (int) doubleValue;
+		System.out.println(intValue);
+		
+	}
+
+}
