08fa63f384adb5aa8de54ab04a46bc59b5022c60
hjs8023 <85880264+hjs8023@users.noreply.github.com>
2021-08-19 10:49:07 +0900

    Update 바깥쪽_반복문종료_.java

--- 혼공자/src/sec04/sec02/exam09/바깥쪽_반복문종료_.java
+++ 혼공자/src/sec04/sec02/exam09/바깥쪽_반복문종료_.java
@@ -7,8 +7,8 @@ public class 바깥쪽_반복문종료_ {
 		Outter: for(char upper ='A'; upper<='Z'; upper++) {		// 바깥쪽 FOR문
 			for(char lower = 'a'; lower<='z'; lower++) {		// 안쪽 FOR문
 				System.out.println(upper + "-" + lower);
-				if(lower =='g') {
-					break Outter;
+				if(lower =='g') {				// 안쪽 FOR문이 g가 되면 
+					break Outter;				// 바깥쪽 FOR문 빠져나와서 종료
 				}
 			}
 		}
