542aa1268684adf8c34fbc0088d0f8b0e2b44976
hjs8023 <85880264+hjs8023@users.noreply.github.com>
2021-08-19 10:48:16 +0900

    Update 바깥쪽_반복문종료_.java

--- 혼공자/src/sec04/sec02/exam09/바깥쪽_반복문종료_.java
+++ 혼공자/src/sec04/sec02/exam09/바깥쪽_반복문종료_.java
@@ -4,8 +4,8 @@ public class 바깥쪽_반복문종료_ {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Outter: for(char upper ='A'; upper<='Z'; upper++) {
-			for(char lower = 'a'; lower<='z'; lower++) {
+		Outter: for(char upper ='A'; upper<='Z'; upper++) {		// 바깥쪽 FOR문
+			for(char lower = 'a'; lower<='z'; lower++) {		// 안쪽 FOR문
 				System.out.println(upper + "-" + lower);
 				if(lower =='g') {
 					break Outter;
