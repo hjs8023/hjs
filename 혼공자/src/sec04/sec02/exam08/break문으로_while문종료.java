690e11f4e9eb8bec3fedceab4de789623c2f3bee
hjs8023 <85880264+hjs8023@users.noreply.github.com>
2021-08-19 10:47:27 +0900

    Update break문으로_while문종료.java

--- 혼공자/src/sec04/sec02/exam08/break문으로_while문종료.java
+++ 혼공자/src/sec04/sec02/exam08/break문으로_while문종료.java
@@ -4,7 +4,7 @@ public class break문으로_while문종료 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		while(true) {
+		while(true) {			// 무한루프
 			int num = (int) (Math.random() * 6) + 1;
 			System.out.println(num);
 			if(num ==6) {
